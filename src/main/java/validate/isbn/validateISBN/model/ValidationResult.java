04071c58825f43dffe9edf4c9755ce54f0f4cf0e
plante-brendan <plante.brendan@gmail.com>
2022-11-21 12:29:07 -0800

    Lombok calls removed due to difficulty cloning.

--- src/main/java/validate/isbn/validateISBN/model/ValidationResult.java
+++ src/main/java/validate/isbn/validateISBN/model/ValidationResult.java
@@ -1,15 +1,20 @@
 package validate.isbn.validateISBN.model;
-
-import jdk.jfr.DataAmount;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 
-@Data
-@AllArgsConstructor
 public class ValidationResult implements Serializable {
     public Boolean valid;
     public String message;
+
+    public ValidationResult(boolean valid, String message) {
+        this.valid = valid;
+        this.message = message;
+    }
+
+    public Boolean getValid() {
+        return valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
